94a6e0b116a9a6a1335cf6816aa1e7e58a36cf75
LauraPastor <laurapastorj@gmail.com>
2025-11-03 21:35:01 +0100

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+
+        <h1 className="text-3xl font-bold underline bg-blue-500">
+          Hello world!!!!!!!!!!!1
+        </h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
