b5032b023471dc1bb706be944cf5ec48cd87ae7e
LauraPastor <laurapastorj@gmail.com>
2026-01-05 10:22:04 +0100

    Load More-Less button in PerfumeGrid

--- src/components/PerfumeGrid.tsx
+++ src/components/PerfumeGrid.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPerfumes } from "../store/perfumeSlice";
 import type { RootState, AppDispatch } from "../store";
@@ -7,15 +7,21 @@ import { addToCart } from "../store/cartSlice";
 const PerfumeGrid = () => {
     const dispatch = useDispatch<AppDispatch>();
     const { filteredPerfumes: perfumes, status } = useSelector((state: RootState) => state.perfumes);
+    const ITEMS_PER_PAGE = 16;
 
+
+    const [visibleCount, setVisibleCount] = useState(ITEMS_PER_PAGE);
+    const visiblePerfumes = perfumes.slice(0, visibleCount);
+    const hasMore = visibleCount < perfumes.length;
+    const isExpanded = visibleCount >= perfumes.length;
     useEffect(() => {
         dispatch(fetchPerfumes());
     }, [dispatch]);
 
     if (status === "loading") return <p className="text-center mt-10">Loading...</p>;
     if (status === "failed") return <p className="text-center mt-10 text-red-500">Failed to load perfumes.</p>;
-    return <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {perfumes.map(perfume => (
+    return <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {visiblePerfumes.map((perfume) => (
             <div key={perfume.id} className="bg-white rounded-2xl shadow-md overflow-hidden transform transition duration-300 hover:-translate-y-1 hover:shadow-xl cursor-pointer text-red-300">
                 <img src={perfume.image} alt={perfume.name} className="h-56 w-full object-cover" />
                 <div className="p-4">
@@ -38,7 +44,28 @@ const PerfumeGrid = () => {
                 </div>
             </div>
         ))}
+        <div className="col-span-full flex justify-center mt-8 gap-4">
+            {hasMore && (
+                <button
+                    onClick={() => setVisibleCount(v => v + ITEMS_PER_PAGE)}
+                    className="px-6 py-2 border rounded-md text-sm hover:bg-gray-100 transition"
+                >
+                    Load more
+                </button>
+            )}
+
+            {isExpanded && perfumes.length > ITEMS_PER_PAGE && (
+                <button
+                    onClick={() => setVisibleCount(ITEMS_PER_PAGE)}
+                    className="px-6 py-2 border rounded-md text-sm hover:bg-gray-100 transition"
+                >
+                    Show less
+                </button>
+            )}
+        </div>
+
     </div>;
+
 };
 
 export default PerfumeGrid;
